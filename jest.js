9fec7994628bd5339bc2b1575fa6a8dbf829528c
MacMini <181009546+veles-rgb@users.noreply.github.com>
2025-06-02 17:51:36 -0400

    Complete calculator and caesarCipher

--- jest.js
+++ jest.js
@@ -10,5 +10,80 @@ function reverseString(str) {
     return str.split('').reverse().join('');
 }
 
+// calculator
+const calculator = {
+    add: function (num1, num2) {
+        return num1 + num2;
+    },
+    subtract: function (num1, num2) {
+        return num1 - num2;
+    },
+    divide: function (num1, num2) {
+        if (num2 !== 0) return num1 / num2;
+        return `Cannot divide by 0`;
+    },
+    multiply: function (num1, num2) {
+        return num1 * num2;
+    }
+};
+
+// Modular function (for caesarCipher)
+function mod(n, p) {
+    let r = n % p;
+    if (r < 0) {
+        r += p;
+    }
+    return r;
+}
+
+// caesarCipher
+function caesarCipher(str, shiftFactor) {
+    let newStr = "";
+
+    for (let letter of str) {
+        // Get the ASCII code (number)
+        let charCode = letter.codePointAt(0);
+
+        // Capital letters
+        if (letter === letter.toUpperCase()) {
+            // ASCII Code of letter A (65)
+            const codeA = "A".codePointAt(0);
+            // ASCII Code of letter Z (	90)
+            const codeZ = "Z".codePointAt(0);
+
+            // If a letter between A-Z (uppercase)
+            if (charCode >= codeA && charCode <= codeZ) {
+                // Remove 65 from charCode (0-25)
+                charCode -= codeA;
+                // Wrap around using modular
+                charCode = mod(charCode + shiftFactor, 26);
+                // Add 65 back to charCode
+                charCode += codeA;
+            }
+        }
+        // Lowercase letters 
+        else if (letter !== letter.toUpperCase()) {
+            // ASCII Code of letter A (97)
+            const codeA = "a".codePointAt(0);
+            // ASCII Code of letter Z (122)
+            const codeZ = "z".codePointAt(0);
+
+            // If a letter between a-z (lowercase)
+            if (charCode >= codeA && charCode <= codeZ) {
+                // Remove 97 from charCode (0-25)
+                charCode -= codeA;
+                // Wrap around using modular
+                charCode = mod(charCode + shiftFactor, 26);
+                // Add 97 back to charCode
+                charCode += codeA;
+            }
+        }
+        // Turn charCode back to a letter and add to newStr
+        newStr += String.fromCodePoint(charCode);
+    }
+
+    return newStr;
+};
+
 // Exports
-export { capitalize, reverseString };
+export { capitalize, reverseString, calculator, caesarCipher };
--- jest.test.js
+++ jest.test.js
@@ -1,7 +1,7 @@
 // jest.test.js
 
 // Imports
-import { capitalize, reverseString } from "./jest.js";
+import { capitalize, reverseString, calculator, caesarCipher } from "./jest.js";
 
 // capitalize test
 test('First letter capitalized', () => {
@@ -16,4 +16,49 @@ test('Reversed string', () => {
     const reversedStr = reverseString(str);
     console.log(reversedStr);
     expect(reversedStr).toBe(str.split('').reverse().join(''));
+});
+
+// calculator test
+describe("Calculator", () => {
+    test("Adds two given numbers", () => {
+        const num1 = 9, num2 = 10;
+        const result = calculator.add(num1, num2);
+        console.log(result);
+        expect(result).toEqual(num1 + num2);
+    });
+    test("Subtracts two given numbers", () => {
+        const num1 = 25, num2 = 4;
+        const result = calculator.subtract(num1, num2);
+        console.log(result);
+        expect(result).toEqual(num1 - num2);
+    });
+    describe("Divide two given numbers unless zero", () => {
+        test("Divide two given numbers", () => {
+            const num1 = 6, num2 = 2;
+            const result = calculator.divide(num1, num2);
+            console.log(result);
+            expect(result).toEqual(num1 / num2);
+        });
+        test('Do not divide by 0', () => {
+            const num1 = 10, num2 = 0;
+            const result = calculator.divide(num1, num2);
+            console.log(result);
+            expect(result).toBe("Cannot divide by 0");
+        });
+    });
+    test("Multiply two given numbers", () => {
+        const num1 = 3, num2 = 6;
+        const result = calculator.multiply(num1, num2);
+        console.log(result);
+        expect(result).toEqual(num1 * num2);
+    });
+});
+
+// caesarCipher test
+test("Caesar Cipher", () => {
+    const str = "HELLO, World!";
+    const shiftFactor = 10;
+    const result = caesarCipher(str, shiftFactor);
+    console.log(result);
+    expect(result).toBe("ROVVY, Gybvn!");
 });
